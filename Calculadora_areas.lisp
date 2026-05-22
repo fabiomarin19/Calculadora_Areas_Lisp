0fcf640366f0b044a3fbf410f76c9c08c4d446f9
fabio <fabio.mariny@unmsm.edu.pe>
2026-05-21 20:25:58 -0500

    v3

--- Calculadora_areas.lisp
+++ Calculadora_areas.lisp
@@ -1,5 +1,4 @@
 (format t "~%========== CALCULADORA LISP ==========")
-
 (format t "~%1. Suma")
 (format t "~%2. Resta")
 (format t "~%3. Multiplicacion")
@@ -8,66 +7,67 @@
 (format t "~%6. Area de rectangulo")
 
 (format t "~%~%Seleccione una opcion: ")
-(defparameter opcion (read))
+(force-output)
 
-(cond
+(let ((opcion (read)))
 
- ((= opcion 1)
-  (format t "~%Ingrese el primer numero: ")
-  (defparameter a (read))
+  (cond
 
-  (format t "Ingrese el segundo numero: ")
-  (defparameter b (read))
+   ;; SUMA
+   ((= opcion 1)
+    (format t "~%Ingrese el primer numero: ")
+    (force-output)
+    (let ((a (read)))
+      (format t "Ingrese el segundo numero: ")
+      (force-output)
+      (let ((b (read)))
+        (format t "~%Resultado de la suma: ~A~%" (+ a b)))))
 
-  (format t "~%Resultado de la suma: ~A~%" (+ a b))
- )
+   ;; RESTA
+   ((= opcion 2)
+    (format t "~%Ingrese el primer numero: ")
+    (force-output)
+    (let ((a (read)))
+      (format t "Ingrese el segundo numero: ")
+      (force-output)
+      (let ((b (read)))
+        (format t "~%Resultado de la resta: ~A~%" (- a b)))))
 
- ((= opcion 2)
-  (format t "~%Ingrese el primer numero: ")
-  (defparameter a (read))
+   ;; MULTIPLICACION
+   ((= opcion 3)
+    (format t "~%Ingrese el primer numero: ")
+    (force-output)
+    (let ((a (read)))
+      (format t "Ingrese el segundo numero: ")
+      (force-output)
+      (let ((b (read)))
+        (format t "~%Resultado de la multiplicacion: ~A~%" (* a b)))))
 
-  (format t "Ingrese el segundo numero: ")
-  (defparameter b (read))
+   ;; DIVISION
+   ((= opcion 4)
+    (format t "~%Ingrese el primer numero: ")
+    (force-output)
+    (let ((a (read)))
+      (format t "Ingrese el segundo numero: ")
+      (force-output)
+      (let ((b (read)))
+        (format t "~%Resultado de la division: ~A~%" (/ a b)))))
 
-  (format t "~%Resultado de la resta: ~A~%" (- a b))
- )
+   ;; AREA CUADRADO
+   ((= opcion 5)
+    (format t "~%Ingrese el lado del cuadrado: ")
+    (force-output)
+    (let ((lado (read)))
+      (format t "~%Area del cuadrado: ~A~%" (* lado lado))))
 
- ((= opcion 3)
-  (format t "~%Ingrese el primer numero: ")
-  (defparameter a (read))
-
-  (format t "Ingrese el segundo numero: ")
-  (defparameter b (read))
-
-  (format t "~%Resultado de la multiplicacion: ~A~%" (* a b))
- )
-
- ((= opcion 4)
-  (format t "~%Ingrese el primer numero: ")
-  (defparameter a (read))
-
-  (format t "Ingrese el segundo numero: ")
-  (defparameter b (read))
-
-  (format t "~%Resultado de la division: ~A~%" (/ a b))
- )
-
- ((= opcion 5)
-  (format t "~%Ingrese el lado del cuadrado: ")
-  (defparameter lado (read))
-
-  (format t "~%Area del cuadrado: ~A~%" (* lado lado))
- )
-
- ((= opcion 6)
-  (format t "~%Ingrese la base del rectangulo: ")
-  (defparameter base (read))
-
-  (format t "Ingrese la altura del rectangulo: ")
-  (defparameter altura (read))
-
-  (format t "~%Area del rectangulo: ~A~%" (* base altura))
- )
- (t
-  (format t "~%Opcion no valida~%"))
-)
+   ;; AREA RECTANGULO
+   ((= opcion 6)
+    (format t "~%Ingrese la base del rectangulo: ")
+    (force-output)
+    (let ((base (read)))
+      (format t "Ingrese la altura del rectangulo: ")
+      (force-output)
+      (let ((altura (read)))
+        (format t "~%Area del rectangulo: ~A~%" (* base altura)))))
+   (t
+    (format t "~%Opcion no valida~%"))))
